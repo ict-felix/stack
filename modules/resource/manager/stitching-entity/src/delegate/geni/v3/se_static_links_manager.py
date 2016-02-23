9f9a28e2f7666f24e7b01ccc06f1b229382f7e7c
felix-jenkins <krzysztof.dombek@gmail.com>
2016-02-23 16:32:50 +0100

    Automatic merge of 'stitching-entity' (efb72a3) into 'master'  > Introducing new Rspec format while still keep compatibility with the old format

--- modules/resource/manager/stitching-entity/src/delegate/geni/v3/se_static_links_manager.py
+++ modules/resource/manager/stitching-entity/src/delegate/geni/v3/se_static_links_manager.py
@@ -1,5 +1,6 @@
 from delegate.geni.v3.db_manager_se import db_sync_manager
 import core
+import se_configurator as SEConfigurator
 
 logger = core.log.getLogger("se-static-link-manager")
 
@@ -10,6 +11,7 @@ class StaticLinkVlanManager(object):
         # TODO: change this to minimise the query result size
         self.getVlansForPorts = db_sync_manager.get_resources()
         self.vlansForStaticLinks = {}
+        self.SEResources = SEConfigurator.seConfigurator()
 
     def getVlanForInterface(self, urn):
         return self.vlansForStaticLinks[urn]
@@ -17,13 +19,21 @@ class StaticLinkVlanManager(object):
     def getVlanMapping(self):
         return self.vlansForStaticLinks
 
-    def chooseVlan(self, port):
-        vlansPorts = self.getVlansForPorts
-        for vlan in vlansPorts[port]:
-            if vlansPorts[port][vlan] == True:
-                logger.info("VLAN %s for static link on port %s has been choosed." %(vlan, port))
+    def chooseVlan(self, port): # TODO: add VLAN choosing   .keys()[0]
+        # get available VLANs for the static link
+        vlans = self.SEResources.get_concrete_port_status(port)
+        # choose the first avaialble VLAN
+        for vlan in vlans:
+            if vlans[vlan] == True:
+                logger.info("Port %s: Choosing VLAN %s for static link" %(port, vlan))
                 self.vlansForStaticLinks[port] = vlan
                 return vlan
+        # vlansPorts = self.getVlansForPorts
+        # for vlan in vlansPorts[port]:
+        #     if vlansPorts[port][vlan] == True:
+        #         logger.info("VLAN %s for static link on port %s has been choosed." %(vlan, port))
+        #         self.vlansForStaticLinks[port] = vlan
+        #         return vlan
 
 
 
