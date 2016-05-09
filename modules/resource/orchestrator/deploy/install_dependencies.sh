bbbd2241e1f15029ea3b616cb3efac571d3135a7
Carolina Fernandez <carolina.fernandez@i2cat.net>
2016-05-09 17:55:55 +0200

    Updating apt dependencies for M/RO

--- modules/resource/orchestrator/deploy/install_dependencies.sh
+++ modules/resource/orchestrator/deploy/install_dependencies.sh
@@ -3,7 +3,7 @@
 echo "Installing RO manageDB dependencies..."
 
 SUDO=`which sudo`
-APT="python-pip mongodb-server python-lxml python-m2crypto python-openssl python-dateutil xmlsec1"
+APT="python-pip mongodb-server python-lxml python-m2crypto python-openssl python-dateutil xmlsec1 libxmlsec1-dev autoconf-archive automake g++ git-core libtool python-dev swig"
 $SUDO apt-get install -y ${APT}
 
 PIP=`which pip`
